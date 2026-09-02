28706f61a9312618741590e1fbdfbc976cb55d20
Augustine Johnson <augustinejohnson@github.com>
2026-09-02 08:14:33 +0100

    Fix distribution missing cross-month orders by adding receivedDate

--- erp-v2/scratch/query_fb.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAw0Q...", // Need to read from firebase.js
-};
-
-// Instead of doing this, I'll just write a script that runs inside the browser context, or I'll just use the app's firebase.js!
